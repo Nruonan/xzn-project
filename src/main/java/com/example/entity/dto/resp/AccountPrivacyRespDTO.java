ab2950141a2d9b71268b273ffac1c8eb596365f8
Nruonan <15231613@qq.com>
2024-09-09 23:44:17 +0800

    实现隐私展示功能

---
+++ src/main/java/com/example/entity/dto/resp/AccountPrivacyRespDTO.java
@@ -0,0 +1,20 @@
+package com.example.entity.dto.resp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Nruonan
+ * @description
+ */
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class AccountPrivacyRespDTO {
+    boolean phone = true;
+    boolean email = true;
+    boolean wx = true;
+    boolean qq = true;
+    boolean gender = true;
+}
