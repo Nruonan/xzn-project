f31b03723917928ba919f56e8d94ebceae4bb61e
Nruonan <15231613@qq.com>
2024-09-25 16:02:46 +0800

    神券下单利用mq异步删除缓存保持数据一致性

--- src/main/java/com/example/listener/TicketQueueListener.java
+++ src/main/java/com/example/listener/TicketQueueListener.java
@@ -1,7 +1,13 @@
 package com.example.listener;
 
+import cn.hutool.core.bean.BeanUtil;
+import com.alibaba.fastjson2.JSONObject;
+import com.example.entity.dao.TicketDO;
+import com.example.entity.dto.resp.TicketRespDTO;
 import com.example.mapper.TicketMapper;
 import com.example.mapper.TicketOrderMapper;
+import com.example.utils.CacheUtils;
+import com.example.utils.Const;
 import com.rabbitmq.client.Channel;
 import jakarta.annotation.Resource;
 import java.io.IOException;
@@ -9,16 +15,22 @@ import java.util.Date;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * @author Nruonan
+ */
 @Slf4j
 @Component
-public class DeadLetterQueueConsumer {
+public class TicketQueueListener {
 
     @Resource
     TicketOrderMapper orderMapper;
-
+    @Resource
+    CacheUtils cacheUtils;
     @Resource
     TicketMapper ticketMapper;
     @RabbitListener(queues = "delay_save_order_queue")
@@ -26,7 +38,7 @@ public class DeadLetterQueueConsumer {
     public void receiveD(Message message, Channel channel) throws IOException {
         String msg = new String(message.getBody());
 
-        int id = Integer.parseInt(msg);
+        long id = Long.parseLong(msg);
         orderMapper.updateTicketCount(id);
         orderMapper.deleteTicketOrder(id);
 
@@ -40,5 +52,14 @@ public class DeadLetterQueueConsumer {
         int id = Integer.parseInt(msg);
         ticketMapper.deleteById(id);
         log.info("当前时间：{},收到死信队列信息{}", new Date(), msg);
+    }
+
+    @RabbitListener(queues = "delete_ticket_count_queue")
+    @Transactional(rollbackFor = Exception.class)
+    @Retryable(value = { Exception.class },maxAttemptsExpression = "${spring.rabbit.listener.simple.retry.max-attempts}",
+        backoff = @Backoff(delayExpression = "${spring.rabbit.listener.simple.retry.back-off.delay}"))
+    public void receiveOrder(String id){
+        cacheUtils.deleteCache(Const.MARKET_TICKET_CACHE + ":" + id);
+        log.info("当前时间：{},删除缓存队列信息{}", new Date(), id);
     }
 }
